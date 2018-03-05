3a2610bc992a7fae7209af22448ea2a9f47c511f
Hashim Sayyid <hashim_sayyid@rocketmail.com>
2018-03-04 20:29:37 -0800

    added comments to rest of my controllers

--- etl-server/src/main/java/com/kollect/etl/controller/MiscellaneousController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/MiscellaneousController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MiscellaneousController {
-	
+
+	/**
+	 * GET request mapping for the misc page to the right html
+	 * @return
+	 */
+
 	  @GetMapping("/misc")
-	  public String datavisualiser() {
+	  public String misc() {
 	    return "misc";
 	  }
 
--- etl-server/src/main/java/com/kollect/etl/controller/UserManagementController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/UserManagementController.java
@@ -17,6 +17,16 @@ public class UserManagementController {
 	@Autowired
 	private UserManagementService userManagementService;
 
+	/**
+	 * GET request to return the user management form, get user by id and populate the table for current users
+	 * @param id
+	 * 			the id needed to return the user details by the id
+	 * @param model
+	 * 				a data structure of objects which needs to be rendered to view
+	 * @return
+	 * 		  returns the usermanagement template
+	 */
+
 	@RequestMapping("/usermanagement")
 	public Object getUserById(@RequestParam(required = false) Integer id, Model model) {
 		model.addAttribute("pageTitle", "DataConnector");
@@ -28,6 +38,28 @@ public class UserManagementController {
 			model.addAttribute("userEditList", null);
 		return "userManagement";
 	}
+
+	/**
+	 * POST request to either add new users or update an existing one if the id is given
+	 * @param id
+	 * 			the id of the user -  sequence that is auto updated
+	 * @param email
+	 * 				email of the user for login
+	 * @param firstName
+	 * 					the first name of the user
+	 * @param lastName
+	 * 					last name of user
+	 * @param password
+	 * 					password of user
+	 * @param enabled
+	 * 				determines whether the user can log in or not
+	 * @param role
+	 * 				the role of the user, either admin or regular user
+	 * @param tenant_id
+	 * 					the id which decides the tenant, i.e. MAHB, PBK, YYC and so on
+	 * @return
+	 * 			redirects to the usermanagement page, with user added to form
+	 */
 
 	@PostMapping("/usermanagement")
 	public String addUser(@RequestParam(required = false) Integer id, @RequestParam String email,
