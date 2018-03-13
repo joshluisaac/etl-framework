ddbcaa49023937e057501b643d678e2ccc5d25c5
Joshua <jnwankwo@acm.org>
2018-03-13 14:20:54 +0800

    Updated etl-dataaccess tests

--- etl-dataaccess/src/test/java/com/kollect/etl/dataaccess/DaoMockTest.java
+++ etl-dataaccess/src/test/java/com/kollect/etl/dataaccess/DaoMockTest.java
@@ -1,40 +1,30 @@
 package com.kollect.etl.dataaccess;
 
-
-
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
-public class MybatisTest {
-
-  private static final Logger LOG = LoggerFactory.getLogger(MybatisTest.class);
+public class DaoMockTest {
+  
+  private static final Logger LOG = LoggerFactory.getLogger(DaoMockTest.class);
   
   private IAbstractSqlSessionProvider daoProvider;
   private Service service;
   
-  //@Mock Dao dao;
-  
   
   @Before
-  public void run_one_per_method() {
+  public void setUp_run_once_per_method() {
     System.out.println("Running mocito....");
     daoProvider = Mockito.mock(AbstractSqlSessionProvider.class);
     service = new Service(daoProvider);
   }
   
   @Test
-  public void test_queryObject() {
+  public void test_query_as400iseries() {
    service.queryObject();
+   LOG.info("Executed Query!");
   }
-
-
 
 }
