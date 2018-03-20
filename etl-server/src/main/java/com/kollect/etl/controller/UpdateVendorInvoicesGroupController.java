b37b45ffe88d5739c0ccba6ce4ae966f13a674cf
yololanda <toin.chang31@gmail.com>
2018-03-20 13:30:04 +0800

    Update Controller, DAO, Service for UpdateVendorInvoices

--- etl-server/src/main/java/com/kollect/etl/controller/UpdateVendorInvoicesGroupController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/UpdateVendorInvoicesGroupController.java
@@ -1,17 +1,23 @@
 package com.kollect.etl.controller;
 
 
+import com.kollect.etl.service.UpdateVendorInvoicesGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class UpdateVendorInvoicesGroupController {
 
-
+    @Autowired
+    private UpdateVendorInvoicesGroupService service;
 
     @PostMapping(value ="/loadvendorinvoicesgroup1")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup1() {
+        service.UpdateVendorInvoicesGroup1(null);
         return "loadvendorinvoicesgroup1";
     }
 
@@ -19,6 +25,7 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup2() {
+        service.UpdateVendorInvoicesGroup2(null);
         return "loadvendorinvoicesgroup2";
     }
 
@@ -26,6 +33,7 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup3() {
+        service.UpdateVendorInvoicesGroup3(null);
         return "loadvendorinvoicesgroup3";
     }
 
@@ -33,6 +41,7 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup4() {
+        service.UpdateVendorInvoicesGroup4(null);
         return "loadvendorinvoicesgroup4";
     }
 
@@ -40,6 +49,7 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup5() {
+        service.UpdateVendorInvoicesGroup5(null);
         return "loadvendorinvoicesgroup5";
     }
 
@@ -47,6 +57,7 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup6() {
+        service.UpdateVendorInvoicesGroup6(null);
         return "loadvendorinvoicesgroup6";
     }
 
@@ -54,6 +65,7 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup7() {
+        service.UpdateVendorInvoicesGroup7(null);
         return "loadvendorinvoicesgroup7";
     }
 
@@ -61,30 +73,35 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup8() {
+        service.UpdateVendorInvoicesGroup8(null);
         return "loadvendorinvoicesgroup8";
     }
     @PostMapping(value ="/loadvendorinvoicesgroup9")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup9() {
+        service.UpdateVendorInvoicesGroup9(null);
         return "loadvendorinvoicesgroup9";
     }
     @PostMapping(value ="/loadvendorinvoicesgroup10")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup10() {
+        service.UpdateVendorInvoicesGroup10(null);
         return "loadvendorinvoicesgroup10";
     }
     @PostMapping(value ="/loadvendorinvoicesgroup11")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup11() {
+        service.UpdateVendorInvoicesGroup11(null);
         return "loadvendorinvoicesgroup11";
     }
     @PostMapping(value ="/loadvendorinvoicesgroup12")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup12() {
+        service.UpdateVendorInvoicesGroup12(null);
         return "loadvendorinvoicesgroup12";
     }
 
