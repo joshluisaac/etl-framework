9c33aa6a6ef97d5aa9a0d19e3efa2d512ddca804
yololanda <toin.chang31@gmail.com>
2018-03-20 17:09:40 +0800

    Update VenoderInvoice's controller or AJAX

--- etl-server/src/main/java/com/kollect/etl/controller/UpdateVendorInvoicesGroupController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/UpdateVendorInvoicesGroupController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class UpdateVendorInvoicesGroupController {
 
@@ -17,92 +19,80 @@ public class UpdateVendorInvoicesGroupController {
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup1() {
-        service.UpdateVendorInvoicesGroup1(null);
-        return "loadvendorinvoicesgroup1";
+        return service.UpdateVendorInvoicesGroup1(null);
     }
 
     @PostMapping(value ="/loadvendorinvoicesgroup2")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup2() {
-        service.UpdateVendorInvoicesGroup2(null);
-        return "loadvendorinvoicesgroup2";
+        return service.UpdateVendorInvoicesGroup2(null);
     }
 
     @PostMapping(value ="/loadvendorinvoicesgroup3")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup3() {
-        service.UpdateVendorInvoicesGroup3(null);
-        return "loadvendorinvoicesgroup3";
+        return service.UpdateVendorInvoicesGroup3(null);
     }
 
     @PostMapping(value ="/loadvendorinvoicesgroup4")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup4() {
-        service.UpdateVendorInvoicesGroup4(null);
-        return "loadvendorinvoicesgroup4";
+        return service.UpdateVendorInvoicesGroup4(null);
     }
 
     @PostMapping(value ="/loadvendorinvoicesgroup5")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup5() {
-        service.UpdateVendorInvoicesGroup5(null);
-        return "loadvendorinvoicesgroup5";
+        return  service.UpdateVendorInvoicesGroup5(null);
     }
 
     @PostMapping(value ="/loadvendorinvoicesgroup6")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup6() {
-        service.UpdateVendorInvoicesGroup6(null);
-        return "loadvendorinvoicesgroup6";
+        return service.UpdateVendorInvoicesGroup6(null);
     }
 
     @PostMapping(value ="/loadvendorinvoicesgroup7")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup7() {
-        service.UpdateVendorInvoicesGroup7(null);
-        return "loadvendorinvoicesgroup7";
+        return service.UpdateVendorInvoicesGroup7(null);
     }
 
     @PostMapping(value ="/loadvendorinvoicesgroup8")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup8() {
-        service.UpdateVendorInvoicesGroup8(null);
-        return "loadvendorinvoicesgroup8";
+        return service.UpdateVendorInvoicesGroup8(null);
     }
     @PostMapping(value ="/loadvendorinvoicesgroup9")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup9() {
-        service.UpdateVendorInvoicesGroup9(null);
-        return "loadvendorinvoicesgroup9";
+        return service.UpdateVendorInvoicesGroup9(null);
     }
     @PostMapping(value ="/loadvendorinvoicesgroup10")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup10() {
-        service.UpdateVendorInvoicesGroup10(null);
-        return "loadvendorinvoicesgroup10";
+        return service.UpdateVendorInvoicesGroup10(null);
     }
     @PostMapping(value ="/loadvendorinvoicesgroup11")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup11() {
-        service.UpdateVendorInvoicesGroup11(null);
-        return "loadvendorinvoicesgroup11";
+        return service.UpdateVendorInvoicesGroup11(null);
     }
     @PostMapping(value ="/loadvendorinvoicesgroup12")
     @SuppressWarnings("unchecked")
     @ResponseBody
     public Object updateVendorInvoicesGroup12() {
-        service.UpdateVendorInvoicesGroup12(null);
-        return "loadvendorinvoicesgroup12";
+        return service.UpdateVendorInvoicesGroup12(null);
     }
 
 }
