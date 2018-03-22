0b808320dcb3dffc8fd7ecf08671035a13cd4937
Hashim Sayyid <hashim_sayyid@rocketmail.com>
2018-03-22 01:31:47 -0700

    moved out the logic from the usermanagement controller to its service

--- etl-server/src/main/java/com/kollect/etl/controller/UserManagementController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/UserManagementController.java
@@ -1,7 +1,5 @@
 package com.kollect.etl.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.kollect.etl.entity.User;
 import com.kollect.etl.service.UserManagementService;
 
 @Controller
@@ -29,14 +26,7 @@ public class UserManagementController {
 
 	@RequestMapping("/usermanagement")
 	public Object getUserById(@RequestParam(required = false) Integer id, Model model) {
-		model.addAttribute("pageTitle", "DataConnector");
-		model.addAttribute("userList", this.userManagementService.viewUser(null));
-		List<User> users = this.userManagementService.getUserById(id);
-		if (users.size() > 0)
-			model.addAttribute("userEditList", users.get(0));
-		else
-			model.addAttribute("userEditList", null);
-		return "userManagement";
+		return this.userManagementService.getUser(id, model);
 	}
 
 	/**
@@ -62,23 +52,11 @@ public class UserManagementController {
 	 */
 
 	@PostMapping("/usermanagement")
-	public String addUser(@RequestParam(required = false) Integer id, @RequestParam String email,
+	public Object addUser(@RequestParam(required = false) Integer id, @RequestParam String email,
 			@RequestParam String firstName, @RequestParam String lastName, @RequestParam String password,
 			@RequestParam(required = false) boolean enabled, @RequestParam String role,
 			@RequestParam Integer tenant_id) {
-
-		boolean insertFlag = false;
-		User newUser = new User(email, firstName, lastName, password, enabled, role, tenant_id);
-		if (id != null)
-			newUser.setId(id);
-		int updateCount = userManagementService.updateUser(newUser);
-		if (updateCount == 0) {
-			this.userManagementService.insertUser(newUser);
-			insertFlag = true;
-		}
-		if (insertFlag)
-			return "redirect:/usermanagement";
-		return "redirect:/usermanagement?id=" + id;
+	    return this.userManagementService.addUpdateUser(id, email, firstName, lastName, password, enabled, role, tenant_id);
 	}
 
 }
--- etl-server/src/main/java/com/kollect/etl/service/UserManagementService.java
+++ etl-server/src/main/java/com/kollect/etl/service/UserManagementService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.kollect.etl.dataaccess.UserDao;
 import com.kollect.etl.entity.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class UserManagementService {
@@ -31,4 +33,32 @@ public class UserManagementService {
     return this.userDao.getUserById(object);
   }
 
+  public Object getUser(@RequestParam(required = false) Integer id, Model model){
+      model.addAttribute("pageTitle", "DataConnector");
+      model.addAttribute("userList", this.viewUser(null));
+      List<User> users = this.getUserById(id);
+      if (users.size() > 0)
+          model.addAttribute("userEditList", users.get(0));
+      else
+          model.addAttribute("userEditList", null);
+      return "userManagement";
+  }
+
+  public Object addUpdateUser(@RequestParam(required = false) Integer id, @RequestParam String email,
+                       @RequestParam String firstName, @RequestParam String lastName, @RequestParam String password,
+                       @RequestParam(required = false) boolean enabled, @RequestParam String role,
+                       @RequestParam Integer tenant_id){
+      boolean insertFlag = false;
+      User newUser = new User(email, firstName, lastName, password, enabled, role, tenant_id);
+      if (id != null)
+          newUser.setId(id);
+      int updateCount = this.updateUser(newUser);
+      if (updateCount == 0) {
+          this.insertUser(newUser);
+          insertFlag = true;
+      }
+      if (insertFlag)
+          return "redirect:/usermanagement";
+      return "redirect:/usermanagement?id=" + id;
+  }
 }
