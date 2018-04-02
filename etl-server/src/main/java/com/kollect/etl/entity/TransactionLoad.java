4d3886badfa6566981e12320faf6afc7f11fce14
Joshua <jnwankwo@acm.org>
2018-04-02 17:16:09 +0800

    Updated transaction load domain driven object

--- etl-server/src/main/java/com/kollect/etl/entity/TransactionLoad.java
+++ etl-server/src/main/java/com/kollect/etl/entity/TransactionLoad.java
@@ -7,7 +7,27 @@ public class TransactionLoad {
   String accountNo;
   boolean isCommercial, invoice, creditNote;
   
+  String line_of_business, invoice_no, invoice_reference;
   
+  
+  public String getLine_of_business() {
+    return line_of_business;
+  }
+  public void setLine_of_business(String line_of_business) {
+    this.line_of_business = line_of_business;
+  }
+  public String getInvoice_no() {
+    return invoice_no;
+  }
+  public void setInvoice_no(String invoice_no) {
+    this.invoice_no = invoice_no;
+  }
+  public String getInvoice_reference() {
+    return invoice_reference;
+  }
+  public void setInvoice_reference(String invoice_reference) {
+    this.invoice_reference = invoice_reference;
+  }
   public int getId() {
     return id;
   }
