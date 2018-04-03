ce90a609aebfda09888357faddda3e218c041500
Hashim Sayyid <hashim_sayyid@rocketmail.com>
2018-04-03 15:38:36 +0800

    refactored the method name process

--- etl-server/src/main/java/com/kollect/etl/controller/TransactionUpdateController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/TransactionUpdateController.java
@@ -23,44 +23,44 @@ import java.util.Map;
 @Controller
 public class TransactionUpdateController {
 
-  //private static final Logger LOG = LoggerFactory.getLogger(TransactionUpdateController.class);
-  private IReadWriteServiceProvider rwProvider;
-  private IAsyncExecutorService executorService;
-  private BatchHistoryService batchHistoryService;
-  private static final Map<String, CrudProcessHolder> MAP = new HashMap<>();
+    //private static final Logger LOG = LoggerFactory.getLogger(TransactionUpdateController.class);
+    private IReadWriteServiceProvider rwProvider;
+    private IAsyncExecutorService executorService;
+    private BatchHistoryService batchHistoryService;
+    private static final Map<String, CrudProcessHolder> MAP = new HashMap<>();
 
-  @Autowired
-  public TransactionUpdateController(IReadWriteServiceProvider rwProvider, IAsyncBatchService asyncService,
-      BatchHistoryService batchHistoryService, IAsyncExecutorService executorService) {
-    this.rwProvider = rwProvider;
-    this.executorService = executorService;
-    this.batchHistoryService = batchHistoryService;
-    BatchConfig.buildHolderMap(MAP);
-  }
+    @Autowired
+    public TransactionUpdateController(IReadWriteServiceProvider rwProvider, IAsyncBatchService asyncService,
+                                       BatchHistoryService batchHistoryService, IAsyncExecutorService executorService) {
+        this.rwProvider = rwProvider;
+        this.executorService = executorService;
+        this.batchHistoryService = batchHistoryService;
+        BatchConfig.buildHolderMap(MAP);
+    }
 
-  public void process() {
-      for (Map.Entry<String, CrudProcessHolder> entry:  MAP.entrySet()) {
-          CrudProcessHolder holder = entry.getValue();
-          final int thread = holder.getThread();
-          final int commitSize = holder.getCommitSize();
-          final String queryName = holder.getQueryName();
-          final String updateQuery = "updateTransactionLoad";
-          List<TransactionLoad> list = rwProvider.executeQuery(queryName, null);
-          int recordCount = list.size();
-          executorService.invoke(list, updateQuery, thread, commitSize);
-          holder.setRecordCount(recordCount);
-      }
-  }
+    public void cycleAndProcessEntries() {
+        for (Map.Entry<String, CrudProcessHolder> entry : MAP.entrySet()) {
+            CrudProcessHolder holder = entry.getValue();
+            final int thread = holder.getThread();
+            final int commitSize = holder.getCommitSize();
+            final String queryName = holder.getQueryName();
+            final String updateQuery = "updateTransactionLoad";
+            List<TransactionLoad> list = rwProvider.executeQuery(queryName, null);
+            int recordCount = list.size();
+            executorService.invoke(list, updateQuery, thread, commitSize);
+            holder.setRecordCount(recordCount);
+        }
+    }
 
-  @PostMapping(value = "/updateTrxLoadInvoicesByDocType")
-  @ResponseBody
-  public Object updateInvoicesByDocType(@RequestParam(required = false) Integer batch_id) {
-    long startTime = System.nanoTime();
-    process();
-    long endTime = System.nanoTime();
-    long timeTaken = (endTime - startTime) / 1000000;
-    this.batchHistoryService.runBatchHistory(batch_id, 0, timeTaken);
-    return 0;
-  }
+    @PostMapping(value = "/updateTrxLoadInvoicesByDocType")
+    @ResponseBody
+    public Object updateInvoicesByDocType(@RequestParam(required = false) Integer batch_id) {
+        long startTime = System.nanoTime();
+        cycleAndProcessEntries();
+        long endTime = System.nanoTime();
+        long timeTaken = (endTime - startTime) / 1000000;
+        this.batchHistoryService.runBatchHistory(batch_id, 0, timeTaken);
+        return 0;
+    }
 
 }
