dc4943444ff2ea0f364b61b877c2ce82409b7c50
Hashim Sayyid <hashim_sayyid@rocketmail.com>
2018-04-03 15:41:13 +0800

    removed unused imports

--- etl-server/src/main/java/com/kollect/etl/controller/TransactionUpdateController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/TransactionUpdateController.java
@@ -5,17 +5,12 @@ import com.kollect.etl.config.CrudProcessHolder;
 import com.kollect.etl.entity.TransactionLoad;
 import com.kollect.etl.service.*;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
