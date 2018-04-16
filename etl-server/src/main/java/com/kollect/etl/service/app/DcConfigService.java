07ba22f22bab080a6bb9779faf1fb5b79519a9b6
Hashim Sayyid <hashim_sayyid@rocketmail.com>
2018-04-16 17:40:17 +0800

    removed redundant DAO from DCConfig and used the rwService Provider

--- etl-server/src/main/java/com/kollect/etl/service/app/DcConfigService.java
+++ etl-server/src/main/java/com/kollect/etl/service/app/DcConfigService.java
@@ -1,8 +1,9 @@
 package com.kollect.etl.service.app;
 
-import com.kollect.etl.dataaccess.DcConfigDao;
 import com.kollect.etl.entity.app.DcConfig;
+import com.kollect.etl.service.IReadWriteServiceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,43 +11,43 @@ import java.util.List;
 @Service
 public class DcConfigService {
 
-  @Autowired
-  private DcConfigDao dcConfigDao;
-  
-  
-  public List<DcConfig> getDcConfigById(Object object){
-    return this.dcConfigDao.getDcConfigById(object);
-  }
-  
-  public List<DcConfig> getDcConfigByParentId(Object object){
-    return this.dcConfigDao.getDcConfigByParentId(object);
-  }
-  
-  
-  public List<Object> getDcConfigParent(Object obj){
-    return this.dcConfigDao.getDcConfigParent(obj);
-  }
+    @Autowired
+    private IReadWriteServiceProvider rwProvider;
+    @Value("${app.datasource_uat_8}")
+    String dataSource;
+
+    public List<DcConfig> getDcConfigById(Object object) {
+        return this.rwProvider.executeQuery(dataSource, "getDcConfigById", object);
+    }
+
+    public List<DcConfig> getDcConfigByParentId(Object object) {
+        return this.rwProvider.executeQuery(dataSource, "getDcConfigByParentId", object);
+    }
+
+    public List<Object> getDcConfigParent(Object object) {
+        return this.rwProvider.executeQuery(dataSource, "getDcConfigParent", object);
+    }
 
 
-  public int deleteDcConfig(Object object) {
-    return this.dcConfigDao.deleteConfig(object);
-  }
-  
-  public int saveConfig(Object object) {
-    return this.dcConfigDao.saveConfig(object);
-  }
-  
-  public int updateDcConfigProp(Object object) {
-    return this.dcConfigDao.updateDcConfigProp(object);
-}
-  
-  public int insertDcConfigProp(Object object) {
-	  return this.dcConfigDao.insertDcConfigProp(object);
-  }
-  
-  public List<Object> getConfigListForExport(Object obj){
-    return this.dcConfigDao.getConfigListForExport(obj);
-  }
-  
-  
+    public int deleteDcConfig(Object object) {
+        return this.rwProvider.insertQuery(dataSource, "deleteDcConfig", object);
+    }
+
+    public int saveConfig(Object object) {
+        return this.rwProvider.insertQuery(dataSource, "insertDcConfig", object);
+    }
+
+    public int updateDcConfigProp(Object object) {
+        return this.rwProvider.updateQuery(dataSource, "updateDcConfigProp", object);
+    }
+
+    public int insertDcConfigProp(Object object) {
+        return this.rwProvider.insertQuery(dataSource, "insertDcConfigProp", object);
+    }
+
+    public List<Object> getConfigListForExport(Object obj) {
+        return this.rwProvider.executeQuery(dataSource, "getConfigListForExport", obj);
+    }
+
+
 }
