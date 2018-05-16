d70a1703169c27147cd3a8d80b36ecf70d14d5ba
MohRichard <38385202+MohRichard@users.noreply.github.com>
2018-05-16 15:59:09 +0800

    add comment in AdminDatabaseConfigController

--- etl-server/src/main/java/com/kollect/etl/controller/app/AdminDatabaseConfigController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/app/AdminDatabaseConfigController.java
@@ -24,11 +24,23 @@ public class AdminDatabaseConfigController {
 	@Autowired
 	private AdminDatabaseConfigService service;
 
+	/**
+	 * GET request to modify the page title in the adminDatabase form/html.
+	 *
+	 * @return
+     */
+
 	@RequestMapping("/adminDatabase")
 	public String adminDatabase(Model model) {
 		model.addAttribute("pageTitle", "Admin - Database Settings");
 		return "adminDatabase";
 	}
+
+	/**
+     * GET request to display the amount data inside the Admin Database.
+     *
+     * If the List (Number of data inside the Database) is bigger than 0, then it will display the amount of the data.
+     */
 
 	@GetMapping(value = "/adminDatabase")
 	public String viewDatabaseSettings(Model model2) {
@@ -41,17 +53,32 @@ public class AdminDatabaseConfigController {
 		    }
 		return "adminDatabase";
 	}
-	
+    /**
+     * HTTP POST request mapping to create or update admin database.
+     *
+     * @param driver
+     *            driver version
+     * @param url
+     *            web address
+     * @param additionalArgs
+     *            extra argument
+     * @param username
+     *            admin username
+     * @param password
+     *            admin password
+     * @param port
+     *            SMTP server port
+     */
+
 	@RequestMapping(value = "/adminDatabaseProp", method = RequestMethod.POST)
 	public String adminDatabaseProp(@RequestParam String driver, @RequestParam String url,
 			@RequestParam String additionalArgs, @RequestParam String username, @RequestParam String password,
 			@RequestParam Integer port) {
-
-		
 		// create new class for AdminDatabaseProp and change all line below
 		AdminDatabaseProp AdminDatabaseprop = new AdminDatabaseProp(driver, url, additionalArgs, username, password,
 				port);
-		
+
+
 
 		 @SuppressWarnings("unchecked")
 		 Map<String, Integer> counterMap = (Map<String, Integer>) service.getAdminDatabaseCounter().get(0);
