475aac76c5ce291ebf83a8d648387f99a5d18f20
Hashim <hashim_sayyid@rocketmail.com>
2018-06-19 16:36:05 +0800

    log error visualizer complete, finishing touches left

--- etl-server/src/main/java/com/kollect/etl/controller/app/LogErrorVisualizerController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/app/LogErrorVisualizerController.java
@@ -6,13 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.FileNotFoundException;
+
 @Controller
 public class LogErrorVisualizerController {
     @Autowired
     private LogErrorVisualizerService logErrorVisualizerService;
 
     @GetMapping("/logerrorvisualizer")
-    public Object viewLogErrorVisualizer(Model model){
+    public Object viewLogErrorVisualizer(Model model) throws FileNotFoundException {
         this.logErrorVisualizerService.readLogFile(model);
         return "logErrorVisualizer";
     }
--- etl-server/src/main/java/com/kollect/etl/service/app/LogErrorVisualizerService.java
+++ etl-server/src/main/java/com/kollect/etl/service/app/LogErrorVisualizerService.java
@@ -3,40 +3,33 @@ package com.kollect.etl.service.app;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 @Service
 public class LogErrorVisualizerService {
-    public void readLogFile(Model model) {
-        try {
-            File file = new File("./etl-server/uploads/stacktrace.log");
-            FileInputStream fStream = new FileInputStream(file);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fStream));
-            String strLine;
-            List<String> causedByList = new ArrayList<>();
-            List<String> reasonList = new ArrayList<>();
-            /* read log line by line */
-            while ((strLine = br.readLine()) != null) {
-                /*Get logs that start with "Caused by:" */
-                if (strLine.startsWith("Caused by:")) {
-                    String parts[] = strLine.split(":", 3);
-                    /* we only need part 2 and 3 */
-                    String part2 = parts[1];
-                    causedByList.add(part2);
-                    String part3 = parts[2];
-                    reasonList.add(part3);
-                    model.addAttribute("part2", causedByList);
-                    model.addAttribute("part3", reasonList);
-                }
-            }
-            fStream.close();
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
+    public void readLogFile(Model model) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("./etl-server/uploads/stacktrace.log"));
+        ArrayList<String> initialList = new ArrayList<>();
+        ArrayList<String> modifiedListPart1 = new ArrayList<>();
+        ArrayList<String> modifiedListPart2 = new ArrayList<>();
+        /* read log line by line */
+        while (scanner.hasNextLine()) {
+            initialList.add(scanner.nextLine());
         }
+        /*Get logs that start with "Caused by:" */
+        for (String aList : initialList) {
+            if (aList.startsWith("Caused by:")) {
+                String[] parts = aList.split(":");
+                /* we only need part 2 and 3 */
+                modifiedListPart1.add(parts[1]);
+                modifiedListPart2.add(parts[2]);
+            }
+        }
+        model.addAttribute("part1", modifiedListPart1);
+        model.addAttribute("part2", modifiedListPart2);
+        scanner.close();
     }
 }
