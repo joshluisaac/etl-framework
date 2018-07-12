94ad6bd763e706dfb121e28f6024c9f4a6e18a19
Hashim <hashim_sayyid@rocketmail.com>
2018-07-12 10:56:45 +0800

    Moved over static content to the template_batch_email.html so that Java does not need to process static info

--- etl-server/src/main/java/com/kollect/etl/service/app/MailContentBuilderService.java
+++ etl-server/src/main/java/com/kollect/etl/service/app/MailContentBuilderService.java
@@ -5,17 +5,11 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Service
 public class MailContentBuilderService {
     private TemplateEngine templateEngine;
-
-    private final SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM, yyyy");
-    private Timestamp today = new Timestamp(System.currentTimeMillis());
-    private String intro ="This is an Automated Notification for KollectValley Batch Statistics for " + sdf.format(today) + ".";
 
     @Autowired
     public MailContentBuilderService(TemplateEngine templateEngine) {
@@ -24,14 +18,8 @@ public class MailContentBuilderService {
 
     public String buildBatchEmail(List<Object> uatStats, List<Object> prodStats) {
         Context context = new Context();
-        context.setVariable("title", "Hi there,");
-        context.setVariable("intro", intro);
-        context.setVariable("message", "Batch Summary & Statistics:");
         context.setVariable("uatStats", uatStats);
         context.setVariable("prodStats", prodStats);
-        context.setVariable("salutation", "Regards,");
-        context.setVariable("signOff", "PowerETL Auto Update");
-        context.setVariable("footer", null);
         return templateEngine.process("fragments/template_batch_mail_template", context);
     }
 }
