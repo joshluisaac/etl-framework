c5133755c5eb6170556bf2162164d25506f3aef1
Josh Uzo <nwankwo.joshua@gmail.com>
2018-08-15 06:56:31 +0800

    Added the components, utilities and helpers required to resolve manifest log files

---
+++ etl-server/src/test/java/EmailHelperTest.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.kollect.etl.component.ComponentProvider;
+import com.kollect.etl.component.EmailHelper;
+
+public class EmailHelperTest {
+  
+  
+  private EmailHelper emailHelper;
+  private ComponentProvider comProvider;
+  String dir = "/home/joshua/Desktop/out";
+  private String cacheFilePath = "/media/joshua/martian/kvworkspace/PowerETL/poweretl-resources/resources/manifestCache.log";
+  
+  
+  public EmailHelperTest() {
+    comProvider = new ComponentProvider();
+    emailHelper = new EmailHelper(comProvider,cacheFilePath);
+  }
+
+  @Test
+  public void aggregateLogFiles_Test() throws IOException {
+    List<String> prefixes = new ArrayList<>(Arrays.asList("stats_manifest_pelita_"));
+    List<String> diff = emailHelper.fetchNewManifestLog(new File(dir), prefixes);
+    System.out.println(diff.toString()); 
+  }
+  
+  @Test
+  public void emailAndPresistToCache_Test() throws IOException {
+    /*Create prefixes*/
+    List<String> prefixes = new ArrayList<>(Arrays.asList("stats_manifest_pelita_"));
+    
+    /*Resolve differences*/
+    List<String> diff = emailHelper.fetchNewManifestLog(new File(dir), prefixes);
+    
+    //email the contents of the differences
+    
+    /*write difference to cache*/
+    emailHelper.persistToCache(diff);
+  }
+  
+}
