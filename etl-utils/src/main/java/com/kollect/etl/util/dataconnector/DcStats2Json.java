1bb0b7b776db69ed14f600c4ac8ebf9d508fa65f
Josh Uzo <nwankwo.joshua@gmail.com>
2018-08-17 13:31:19 +0800

    Added some comments for refactoring

--- etl-utils/src/main/java/com/kollect/etl/util/dataconnector/DcStats2Json.java
+++ etl-utils/src/main/java/com/kollect/etl/util/dataconnector/DcStats2Json.java
@@ -10,11 +10,19 @@ import com.kollect.etl.util.PropertiesUtils;
 public class DcStats2Json {
 
 	public void dcLogtoJson(String[] args) throws IOException {
+	  
+	    //Inverse the dependencies. Let the caller or the client inject whatever it needs
+	  // This should also be moved into DcStats2Json constructor
 		Regex r = new Regex();
 		JsonUtils j = new JsonUtils();
 		Properties prop = new PropertiesUtils().loadPropertiesFileResource("server.properties");
+		
+		//this should go into the main method
 		String daysAgo = prop.getProperty("email.daysAgo");
 		List<TotalLoaded> bodyList = r.getMatchedTokens(args[0], Integer.parseInt(daysAgo));
+		
+		//This should be serialized to a JSON formatted output and written to disk. 
+		//PowerETL notification engine will pick up that JSON fie, process it and the send an email using thyme leaf templates
 		j.toJson(bodyList);
 	}
 }
