2ddc2b140462849afe9e0324e852e69fecedfe84
Hashim <hashim_sayyid@rocketmail.com>
2018-11-13 11:04:24 +0800

    changed db to update both uat and production

--- etl-server/src/main/java/com/kollect/etl/controller/nationwide/NationwideBatchController.java
+++ etl-server/src/main/java/com/kollect/etl/controller/nationwide/NationwideBatchController.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class NationwideBatchController {
     private AsyncBatchExecutorService asyncBatchExecutorService;
     private UpdateDataDateService updateDataDateService;
-    private @Value("${app.datasource_all2}")
+    private @Value("${app.datasource_kv_production}")
     List<String> dataSource;
 
     @Autowired
